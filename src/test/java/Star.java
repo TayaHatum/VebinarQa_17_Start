91699e99ef575bfb983c342f3cf5ef1307e1d242
TayaHatum <21taya21@gmail.com>
2022-11-24 15:10:31 +0200

    added css

---
+++ src/test/java/Star.java
@@ -0,0 +1,88 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class Star {
+    WebDriver wd;
+
+
+    @BeforeMethod
+    public void precondition(){
+        wd=new ChromeDriver();
+        wd.get("file:///Users/tayahatum/Downloads/apk/index.html");
+
+    }
+
+    @Test
+    public void testIndexFile(){
+        // items
+        // item 1
+        WebElement item1 =wd.findElement(By.tagName("li"));
+        wd.findElement(By.cssSelector("li"));
+        // item2
+        List<WebElement> items =  wd.findElements(By.cssSelector("li"));
+        WebElement item2 = items.get(1);
+        wd.findElement(By.cssSelector("#nav li:nth-child(2)"));
+        // item3
+       // List<WebElement> items =  wd.findElements(By.cssSelector("li"));
+        WebElement item3 = items.get(2);
+        wd.findElement(By.cssSelector("#nav li:nth-child(3)"));
+        //item 4
+        WebElement item4 = items.get(3);
+        wd.findElement(By.cssSelector("#nav li:last-child"));
+
+        // form
+
+        // name
+        WebElement inputName = wd.findElement(By.tagName("input"));
+
+        wd.findElement(By.name("name"));
+        wd.findElement(By.cssSelector("[name='name']"));
+        wd.findElement(By.cssSelector("input[name='name']"));
+        // surename
+        List <WebElement> names = wd.findElements(By.cssSelector("input"));
+        WebElement inputSureName = names.get(1);
+        wd.findElement(By.cssSelector("input[name='surename']"));
+
+        // button
+        WebElement buttonSend = wd.findElement(By.className("btn"));
+        buttonSend.click();
+        wd.findElement(By.cssSelector(".btn"));
+
+
+        // table
+         // rows
+        wd.findElement(By.cssSelector("table#country-table>tbody>tr"));
+        wd.findElement(By.cssSelector("#country-table tr:first-child"));
+        wd.findElement(By.cssSelector("#country-table tr:nth-child(2)"));
+        wd.findElement(By.cssSelector("#country-table tr:nth-child(3)"));
+        wd.findElement(By.cssSelector("#country-table tr:last-child"));
+
+        // td Germany
+        //String  text = wd.findElement(By.cssSelector("#country-table tr:nth-child(3)>td:first-child")).getText();
+        String  text = wd.findElement(By.cssSelector("tr:nth-child(3)>td:first-child")).getText();
+        Assert.assertEquals(text,"Germany");
+
+    }
+    @Test
+    public void isItem2Select(){
+        // step 1 find item2 + click
+        // Assert  (text from alert , text item2)
+
+        // click by item 2  !=  item 2
+       // Assert.assertTrue(text1.contacns(text2));
+    }
+
+    @AfterMethod
+    public void postCondition(){
+        wd.close();
+      //  wd.quit();
+    }
+}
